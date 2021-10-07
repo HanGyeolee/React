236841f5e2a27feb7ddce7c791e05e00b07b08d5
HanGyeolee <46367614+HanGyeolee@users.noreply.github.com>
2021-10-07 15:40:29 +0900

    Update NewProject.bat

--- Native/NewProject.bat
+++ Native/NewProject.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-echo 새 리액트 네이티브 프로젝트 생성
-set /p name=이름:
+echo Create New React Native Project
+set /p name=Project Name:
 Call mkproj %name%
 echo react-native run-android> %name%\run.bat
